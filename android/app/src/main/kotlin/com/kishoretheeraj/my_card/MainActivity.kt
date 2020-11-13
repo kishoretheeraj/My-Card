36623b3ce67ae97c3ea9ddc19f2ba64a85fe962e
kishoretheeraj <kishoretheeraj@gmail.com>
2020-11-13 14:35:44 +0530

    initial Commit

---
+++ android/app/src/main/kotlin/com/kishoretheeraj/my_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kishoretheeraj.my_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/kishoretheeraj.jpg'),
+              ),
+              Text(
+                'Kishore Theeraj',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
